83ce374f1fd1142edb603ed1b4a33e7e543c0feb
LavoussierDev <Onilr31@gmail.com>
2024-01-11 20:51:25 -0400

    Added multiple files; segmentation fault on file 16

--- V.2/15-binary_tree_is_full.c
+++ V.2/15-binary_tree_is_full.c
@@ -1,1 +1,20 @@
 #include "binary_trees.h"
+
+int binary_tree_is_full(const binary_tree_t *tree) {
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+	else if (tree->left != NULL && tree->right != NULL)
+	{
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	}
+    else
+	{
+		return (0);
+	}
+}
--- V.2/16-binary_tree_is_perfect.c
+++ V.2/16-binary_tree_is_perfect.c
@@ -1,1 +1,24 @@
 #include "binary_trees.h"
+
+int binary_tree_is_perfect(const binary_tree_t *tree) {
+	if (tree == NULL) {
+		return (0);
+	}
+
+	size_t left_height = _height(tree->left);
+	size_t right_height = _height(tree->right);
+
+	if (left_height != right_height)
+	{
+		return (0);
+	}
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return 1;
+	}
+	else
+	{
+		return binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right);
+	}
+}
